0a0baa37e2d1bfc67a210b84e1880638aa134af6
Jacob Hafdahl <jacob.hafdahl@gmail.com>
2015-10-16 14:26:11 -0500

    adding react and requirejs via bower and npm

--- professorstatsmobile/www/js/index.js
+++ professorstatsmobile/www/js/index.js
@@ -16,36 +16,77 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+requirejs.config({
+    paths:{
+        'react': 'lib/react/react.min',
+        'react-dom': 'lib/react/react-dom.min'
     }
-};
+});
 
-app.initialize();
+ requirejs(['react', 'react-dom'], function(React, ReactDOM){
+
+    var data = {
+        name: "World"
+    };
+
+    console.log("React is " + React);
+    var Input = React.createClass({displayName: 'Place',
+        render: function() {
+            return React.DOM.input({ 
+                type: 'text',
+                placeholder: 'some placeholder',
+                value:this.props.name.name
+            });
+        }
+    });
+
+    var Hello = React.createClass({displayName: 'Hello',
+        render: function() {
+            return React.createElement("div", null, "Hello ", this.props.name.name);
+        }
+    });
+     
+    ReactDOM.render(
+        React.createElement(Hello, {name: data}),
+        document.getElementById('container')
+    );
+    ReactDOM.render(
+        React.createElement(Input, {name: data}),
+        document.getElementById('container2')
+    );
+
+    var app = {
+        // Application Constructor
+        initialize: function() {
+            this.bindEvents();
+        },
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        bindEvents: function() {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        },
+        // deviceready Event Handler
+        //
+        // The scope of 'this' is the event. In order to call the 'receivedEvent'
+        // function, we must explicitly call 'app.receivedEvent(...);'
+        onDeviceReady: function() {
+            app.receivedEvent('deviceready');
+        },
+        // Update DOM on a Received Event
+        receivedEvent: function(id) {
+            var parentElement = document.getElementById(id);
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+
+            console.log('Received Event: ' + id);
+        }
+    };
+
+    app.initialize();
+
+});
